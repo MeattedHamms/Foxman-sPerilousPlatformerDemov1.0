d69e3993c52ed0cc644051b5a6310a6813991f0b
MeattedHamms <Jack.Cherniawski@gmail.com>
2025-08-25 19:59:22 -0500

    window size fix

--- index.service.worker.js
+++ index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1756158030|430759886';
+const CACHE_VERSION = '1756169945|77061847';
 /** @type {string} */
 const CACHE_PREFIX = 'Foxman'sPerilous-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
