536729aa70deafa6bd40e0cccf4fc81df0d82865
MeattedHamms <Jack.Cherniawski@gmail.com>
2025-08-25 21:20:53 -0500

    fixpt2

--- index.service.worker.js
+++ index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1756169945|77061847';
+const CACHE_VERSION = '1756174831|189349385';
 /** @type {string} */
 const CACHE_PREFIX = 'Foxman'sPerilous-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
